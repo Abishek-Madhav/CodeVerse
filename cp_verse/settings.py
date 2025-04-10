3c5e2ecc46b03c97b9f8246e1c1be8fe2cb2c2c5
Abishek Madhav <143243139+Abishek-Madhav@users.noreply.github.com>
2025-04-10 16:07:32 +0530

    Update settings.py

--- cp_verse/settings.py
+++ cp_verse/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,7 +83,7 @@ DATABASES = {
 }
 
 database_url=os.environ.get("DATABASE_URL")
-DATABASES["default"]=database_url.parse(database_url)
+DATABASES["default"]=dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
